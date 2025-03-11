3f65772a5f6af565adb99697a2c8e0ea5dd8c51e
nomychanel <nomychanel@gmail.com>
2025-03-11 01:08:37 +0100

    Seguimos probando cositas

--- ej_hechos.py
+++ ej_hechos.py
@@ -4,3 +4,16 @@
 
 
 print("Hola k ase")
+
+# salida de datos calculados
+n_numero1 = 3
+n_numero2 = 5
+n_suma = n_numero1 + n_numero2
+print("El resultado de sumar", n_numero1, "y", n_numero2, "es", n_suma)
+
+
+# Otra manera:
+n_num2 = 3
+n_num3 = 4
+n_suma = n_num2 + n_num3
+print("El total de la suma es", n_suma)
